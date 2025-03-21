8ed8110255af2e180d67cc066e25fa68289aa7e0
Eldan Ben-Haim <eldan@benhaim.net>
2025-03-21 23:01:50 +0200

    Cleanup main

--- software/wordclock/src/main.cpp
+++ software/wordclock/src/main.cpp
@@ -39,6 +39,8 @@ CRGB leds[NUM_LEDS] = {0};
 LedAnimationController ledAnimationController(leds, BOARD_SIZE, BOARD_SIZE);
 WordBoard wordBoard(boardLetters, ledAnimationController);
 
+int lastFrameMillis;
+
 void clearBoardAndShowText(const char *text, int startAtMillis = -1)
 {
   if (startAtMillis == -1)
@@ -49,8 +51,6 @@ void clearBoardAndShowText(const char *text, int startAtMillis = -1)
   ledAnimationController.setAllToColor(CRGB{0, 0, 0}, startAtMillis + 500);
   wordBoard.displayText(text, startAtMillis + 500, NULL);
 }
-
-int lastFrameMillis;
 
 void syncTime()
 {
@@ -67,12 +67,37 @@ void syncTime()
   configTime(TZ_GMTOFFSET_SEC, TZ_DST_OFFSET_SEC, NTP_SERVER);
 
   struct tm timeinfo;
-  if (!getLocalTime(&timeinfo))
+  if (!getLocalTime(&timeinfo, 15000))
   {
     Serial.println("Failed to obtain time");
     return;
   }
+
   Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+
+  WiFi.disconnect();
+}
+
+void refreshBoardTime()
+{
+  int currentHour = 0;
+  int currentMinute = 0;
+
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time; will show random time");
+    currentHour = rand() % 24;
+    currentMinute = rand() % 60;
+  }
+  else
+  {
+    currentHour = timeinfo.tm_hour;
+    currentMinute = timeinfo.tm_min;
+  }
+
+  Serial.printf("Show time %d:%d\n", currentHour, currentMinute);
+  clearBoardAndShowText(timeToString(currentHour, currentMinute).c_str());
 }
 
 void setup()
@@ -90,38 +115,8 @@ void loop()
 
   if (newFrameMillis / (60 * 1000) != lastFrameMillis / (60 * 1000))
   {
-    int hr = rand() % 24;
-    int mt = rand() % 60;
-
-    struct tm timeinfo;
-    if (!getLocalTime(&timeinfo))
-    {
-      Serial.println("Failed to obtain time");
-    }
-    else
-    {
-      hr = timeinfo.tm_hour;
-      mt = timeinfo.tm_min;
-    }
-
-    Serial.printf("Show time %d:%d\n", hr, mt);
-    clearBoardAndShowText(timeToString(hr, mt).c_str());
+    refreshBoardTime();
   }
-  /*
-    if ((newFrameMillis / 10000) % 2 == 0)
-    {
-      if (random() % 100 == 1)
-      {
-        int x = random() % 16;
-        int y = random() % 16;
-
-        ledAnimationController.addAnimation(x, y, {targetColor : CRGB{255, 255, 255}, targetFrame : newFrameMillis + 400});
-        ledAnimationController.addAnimation(x, y, {targetColor : CRGB{128, 0, 0}, targetFrame : newFrameMillis + 800});
-        ledAnimationController.addAnimation(x, y, {targetColor : CRGB{0, 128, 0}, targetFrame : newFrameMillis + 1200});
-        // ledAnimationController.addAnimation(x, y, {targetColor : CRGB{0, 0, 128}, targetFrame : newFrameMillis + 1600});
-        // ledAnimationController.addAnimation(x, y, {targetColor : CRGB{0, 0, 0}, targetFrame : newFrameMillis + 2000});
-      }
-    }*/
 
   ledAnimationController.animate(newFrameMillis);
   lastFrameMillis = newFrameMillis;
